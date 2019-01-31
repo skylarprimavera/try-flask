775d724e36ce40edc7455762fc824ca127decc21
skylarprimavera <47228917+skylarprimavera@users.noreply.github.com>
2019-01-31 14:46:27 -0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+import makeWords
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Sky!'
+
+@app.route('/make/<word>')
+def show_make(word):
+    return repr(makeWords.wordToElems(word))
